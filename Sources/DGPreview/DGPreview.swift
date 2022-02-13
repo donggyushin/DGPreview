c8de028130b6c55e70e568b28a0203fc5dc59cef
donggyu <donggyu_00@naver.com>
2022-02-13 17:31:45 +0900

    [Feature] Make UIKit project enable preview feature of SwiftUI (#2)
    * chore: Edit Package file
    
    * test: remove sample test
    
    * feat: Make UIViewPreview, showPreview
    
    * style: change naming

--- Sources/DGPreview/DGPreview.swift
+++ Sources/DGPreview/DGPreview.swift
@@ -1,6 +1,63 @@
-public struct DGPreview {
-    public private(set) var text = "Hello, World!"
+import SwiftUI
 
-    public init() {
+public struct DGPreview<View: UIView>: UIViewRepresentable {
+    let view: View
+    
+    public init(_ builder: @escaping () -> View) {
+        view = builder()
+    }
+    
+    public func makeUIView(context: Context) -> some UIView {
+        return view
+    }
+    
+    public func updateUIView(_ uiView: UIViewType, context: Context) {
+        view.setContentHuggingPriority(.defaultHigh, for: .horizontal)
+        view.setContentHuggingPriority(.defaultHigh, for: .vertical)
+    }
+}
+
+public enum DeviceType {
+    case iPhoneSE2
+    case iPhone8
+    case iPhone12Pro
+    case iPhone12ProMax
+    case iPhone13Pro
+    case iPhone13ProMax
+
+    func name() -> String {
+        switch self {
+        case .iPhoneSE2:
+            return "iPhone SE"
+        case .iPhone8:
+            return "iPhone 8"
+        case .iPhone12Pro:
+            return "iPhone 12 Pro"
+        case .iPhone12ProMax:
+            return "iPhone 12 Pro Max"
+        case .iPhone13Pro:
+            return "iPhone 13 Pro"
+        case .iPhone13ProMax:
+            return "iPhone 13 Pro Max"
+        }
+    }
+}
+
+import SwiftUI
+public extension UIViewController {
+
+    private struct Preview: UIViewControllerRepresentable {
+        let viewController: UIViewController
+
+        func makeUIViewController(context: Context) -> UIViewController {
+            return viewController
+        }
+
+        func updateUIViewController(_ uiViewController: UIViewController, context: Context) {
+        }
+    }
+
+    func showDGPreview(_ deviceType: DeviceType = .iPhone12Pro) -> some View {
+        Preview(viewController: self).previewDevice(PreviewDevice(rawValue: deviceType.name()))
     }
 }
